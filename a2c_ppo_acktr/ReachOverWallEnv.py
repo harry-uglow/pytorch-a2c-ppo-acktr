daf1a07bfb39d5a01b59ea969b6473b7a8d900e9
Harry Uglow <hu115@ic.ac.uk>
2019-04-08 19:31:49 +0100

    Added DAgger to initial policy training.

--- a2c_ppo_acktr/ReachOverWallEnv.py
+++ a2c_ppo_acktr/ReachOverWallEnv.py
@@ -16,21 +16,44 @@ scene_path = dir_path + '/reach_over_wall.ttt'
 
 class ReachOverWallEnv(VrepEnv):
 
-    def get_demo_path(self):
-        num_path_points = 17
-        _, path, _, _ = self.call_lua_function('solve_ik')
+    def solve_ik(self, group, distance):
+        _, path, _, _ = self.call_lua_function('solve_ik', strings=[group, distance])
+        num_path_points = len(path) // len(self.joint_handles)
         path = np.reshape(path, (num_path_points, len(self.joint_handles)))
         distances = np.array([path[i + 1] - path[i]
                               for i in range(0, len(path) - 1)])
         velocities = distances * 20  # Distances should be covered in 0.05s
-        return path[:-1], velocities
+        return path, velocities
+
+    def get_demo_path(self):
+        path, velocities_WP = self.solve_ik('IK_GroupW', 'tip_waypoint')
+        path_to_WP = path[:-1]
+
+        self.call_lua_function('set_joint_angles', ints=self.init_config_tree, floats=path[-1])
+        path_to_trg, velocities_trg = self.solve_ik('IK_GroupT', 'tip_target')
+
+        return self.normalise_angles(np.append(path_to_WP, path_to_trg[:-1], axis=0)), \
+            np.append(velocities_WP, velocities_trg, axis=0)
 
     def train_initial_policy(self):
-        path_poses, train_y = self.get_demo_path()
-        train_x = self.normalise_angles(path_poses)
-        num_joints = len(self.joint_handles)
-        net = train_nn(InitialPolicy(num_joints, num_joints), train_x, train_y)
-        return net
+        x, y = self.get_demo_path()
+
+        self.initial_policy = train_nn(InitialPolicy(len(x[0]), len(y[0])), x, y)
+
+        null_action = np.array([0.] * len(y[0]))
+        # Use DAgger for 5 episodes
+        for i in range(5):
+            self.reset()
+            done = False
+            while not done:
+                _, _, done, _ = self.step(null_action)
+                # new_x, new_y = self.get_demo_path()
+                new_x, new_y = self.solve_ik('IK_GroupT', 'tip_target')
+                x = np.append(x, self.normalise_angles(new_x[:-1]), axis=0)
+                y = np.append(y, new_y, axis=0)
+
+            p = self.np_random.permutation(len(x))
+            self.initial_policy = train_nn(self.initial_policy, x[p], y[p])
 
     observation_space = spaces.Box(np.array([0] * 12), np.array([1] * 12),
                                    dtype=np.float32)
@@ -41,6 +64,7 @@ class ReachOverWallEnv(VrepEnv):
     target_velocities = np.array([0., 0., 0., 0., 0., 0., 0.])
     target_pose = np.array([0.5, 0., 0.45])
     timestep = 0
+    initial_policy = None
 
     def __init__(self, seed, rank, ep_len=64, headless=True):
         super().__init__(rank, headless)
@@ -67,7 +91,7 @@ class ReachOverWallEnv(VrepEnv):
             self.joint_handles[i] = handle
 
         return_code, self.end_handle = vrep.simxGetObjectHandle(self.cid,
-                "Sawyer_tip", vrep.simx_opmode_blocking)
+                "BaxterGripper_centerJoint", vrep.simx_opmode_blocking)
         check_for_errors(return_code)
         _, self.target_handle = vrep.simxGetObjectHandle(self.cid,
                 "Waypoint", vrep.simx_opmode_blocking)
@@ -81,11 +105,11 @@ class ReachOverWallEnv(VrepEnv):
                 self.wall_handle, -1, vrep.simx_opmode_blocking)[1]
         self.wall_orientation = self.init_wall_rot
 
-        self.initial_policy = self.train_initial_policy()
-
         # Start the simulation (the "Play" button in V-Rep should now be in a "Pressed" state)
         return_code = vrep.simxStartSimulation(self.cid, vrep.simx_opmode_blocking)
         check_for_errors(return_code)
+
+        self.train_initial_policy()
 
     # Normalise target so that x and y are in range [0, 1].
     def normalise_coords(self, coords, lower=np.array([0.2, -0.4, 0.2]),
@@ -120,8 +144,7 @@ class ReachOverWallEnv(VrepEnv):
 
     def step(self, a):
         initial_policy_input = torch.from_numpy(self.normalise_angles(self.joint_angles))
-        initial_policy_action = self.initial_policy(
-            initial_policy_input).detach().numpy()
+        initial_policy_action = self.initial_policy(initial_policy_input).detach().numpy()
         self.target_velocities = initial_policy_action + a  # Residual RL
         vec = self.get_end_pose() - self.target_pose
         reward_dist = - np.linalg.norm(vec)
